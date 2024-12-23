f063c04cd8885ff0ff46477d89d792c26fafe4c1
srijan1805 <srijan@dezyit.com>
2024-12-23 22:52:42 +0530

    Refractor: enhance validation messages for signup and signin fields

--- src/validators/auth.validator.ts
+++ src/validators/auth.validator.ts
@@ -1,14 +1,26 @@
 import { z } from "zod";
 
 export const SignUpValidator = z.object({
-    email: z.string().email(),
-    password: z.string().min(6),
-    name: z.string().min(3),
+    email: z
+        .string({
+            message: "Invalid email",
+        })
+        .email({
+            message: "Invalid email",
+        }),
+    password: z.string().min(6, {
+        message: "Password must be at least 6 characters long",
+    }),
+    name: z.string().min(3, {
+        message: "Name must be at least 3 characters long",
+    }),
 });
 
 export const SignInValidator = z.object({
     email: z.string().email(),
-    password: z.string().min(6),
+    password: z.string().min(6, {
+        message: "Password must be at least 6 characters long",
+    }),
 });
 
 export const VerifyTokenValidator = z.object({
